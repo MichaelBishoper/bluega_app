b3af248691151dfea7be0f152a29297d742d7397
sevnsecn <arthurmatthewpg@gmail.com>
2025-10-19 20:13:30 +0700

    User schema CR
    #installed new dependency Jakarta
    
    #User Schema:
    - CRUD
    - Deleting a user removes that if followed by another user
    - Duplicate checks for username & followids

---
+++ backend/musicplayer/src/main/java/com/musicplayer/musicplayer/controller/UsersController.java
@@ -0,0 +1,53 @@
+package com.musicplayer.musicplayer.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.musicplayer.musicplayer.model.Users;
+import com.musicplayer.musicplayer.service.UsersService;
+@RestController
+@RequestMapping("api/users")
+public class UsersController {
+    @Autowired
+    private UsersService usersService;
+    
+    @GetMapping
+    public List<Users> getAllUsers() {
+        return usersService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public Users getUserById(@PathVariable String id) {
+        return usersService.getUserById(id);
+    }
+
+    @PostMapping
+    public Users addUser(@RequestBody Users user) {
+        return usersService.addUser(user);
+    }
+    @PutMapping("/{id}")
+    public Users updateUser(@PathVariable String id, @RequestBody Users user) {
+    return usersService.updateUser(id, user);
+}
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable String id) {
+        usersService.deleteUser(id);
+    }
+
+    @PutMapping("/{userId}/follow/{targetId}")
+    public Users followUser(@PathVariable String userId, @PathVariable String targetId) {
+    return usersService.followUser(userId, targetId);
+}
+
+
+    
+}
---
+++ backend/musicplayer/src/main/java/com/musicplayer/musicplayer/repository/UsersRepository.java
@@ -0,0 +1,8 @@
+package com.musicplayer.musicplayer.repository;
+import org.springframework.data.mongodb.repository.MongoRepository; //importing MongoRepository
+
+import com.musicplayer.musicplayer.model.Users;
+public interface UsersRepository extends MongoRepository<Users, String> {
+    Users findByUsername(String username); //optional, another option to find with username instead of id
+}
+
