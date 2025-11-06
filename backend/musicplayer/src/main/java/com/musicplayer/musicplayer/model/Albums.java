0a8235d5df40e46e04ae8c99f692eafaf0b4a326
MichaelBishoper <xaverio.chung@student.sgu.ac.id>
2025-11-06 23:48:43 +0700

    Update Albums.java
    Created the full CRUD for Albums
    
    To Do:
    - Add users endpoints to find their created/liked albums
    - Add users endpoints to find their created/liked songs

--- backend/musicplayer/src/main/java/com/musicplayer/musicplayer/model/Albums.java
+++ backend/musicplayer/src/main/java/com/musicplayer/musicplayer/model/Albums.java
@@ -13,14 +13,40 @@ public class Albums {
     private String id;
     private String title;
     private String artist;
-    private List<String> songIds;
+    private List<AlbumSong> songs;
+
+    // Nested class to hold songIDs and ordering within an album
+    public static class AlbumSong {
+        private String songId;
+        private int order;
+
+        public AlbumSong() {}
+        
+        public AlbumSong(String songId, int order) {
+            this.songId = songId;
+            this.order = order;
+        }
+        // Constructors
+        public void setSongId(String songId) {
+            this.songId = songId;
+        }
+        public String getSongId() {
+            return songId;
+        }
+        public void setOrder(int order) {
+            this.order = order;
+        }
+        public int getOrder() {
+            return order;
+        }
+    }
 
     public Albums() {}
 
-    public Albums(String title, String artist, List<String> SongIDs) {
+    public Albums(String title, String artist, List<AlbumSong> songs) {
         this.title = title;
         this.artist = artist;
-        this.songIds = SongIDs;
+        this.songs = songs;
     }
 
     public void setArtist(String artist) {
@@ -41,11 +67,11 @@ public class Albums {
     public String getTitle() {
         return title;
     }
-    public void setSongIds(List<String> songIds) {
-        this.songIds = songIds;
+    public void setSongs(List<AlbumSong> songs) {
+        this.songs = songs;
     }
-    public List<String> getSongIds() {
-        return songIds;
+    public List<AlbumSong> getSongs() {
+        return songs;
     }
 }
 
