5081d6c20fa9a6363e48322e39c078d0ef3d703f
MichaelBishoper <xaverio.chung@student.sgu.ac.id>
2025-11-06 23:54:48 +0700

    Added getAlbum and getSongsFromAlbum

--- backend/musicplayer/src/main/java/com/musicplayer/musicplayer/controller/AlbumsController.java
+++ backend/musicplayer/src/main/java/com/musicplayer/musicplayer/controller/AlbumsController.java
@@ -2,7 +2,7 @@ package com.musicplayer.musicplayer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import com.musicplayer.musicplayer.model.Songs; // Import Songs
 import com.musicplayer.musicplayer.model.Albums;
 import com.musicplayer.musicplayer.service.AlbumsService;
 
@@ -19,5 +19,29 @@ public class AlbumsController {
         return albumsService.getAllAlbums();
     }
 
+    @GetMapping("/{id}")
+    public Albums getAlbumByID(@PathVariable String id) {
+        return albumsService.getAlbum(id);
+    }
+
+    @GetMapping("/{id}/songs") // Album's ID NOT Song's
+    public List<Songs> getSongsFromAlbum(@PathVariable String id) {
+        return albumsService.getSongsFromAlbum(id);
+    }
+
+    @PostMapping
+    public Albums addAlbum(@RequestBody Albums album) {
+        return albumsService.addAlbum(album);
+    }
+
+    @PutMapping("/{id}")
+    public Albums updateAlbum(@PathVariable String id, @RequestBody Albums album) {
+        return albumsService.updateAlbum(id, album);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAlbum(@PathVariable String id) {
+        albumsService.deleteAlbum(id);
+    }
     
 }
