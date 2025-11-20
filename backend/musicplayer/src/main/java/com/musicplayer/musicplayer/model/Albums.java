721dc6dc060b4f34c3ba71affebc9a16bfe6dd5e
MichaelBishoper <xaverio.chung@student.sgu.ac.id>
2025-11-20 14:01:51 +0700

    Create Songs from Albums

--- backend/musicplayer/src/main/java/com/musicplayer/musicplayer/model/Albums.java
+++ backend/musicplayer/src/main/java/com/musicplayer/musicplayer/model/Albums.java
@@ -1,5 +1,5 @@
 package com.musicplayer.musicplayer.model;
-
+import java.util.ArrayList;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.annotation.Id;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -9,79 +9,94 @@ import java.util.List;
 @JsonIgnoreProperties(ignoreUnknown = true) // Prevents mapping errors from unknown fields
 @Document(collection = "albums")
 public class Albums {
+
     @Id
     private String id;
     private String title;
     private String artist;
-    private String type; 
-    private List<AlbumSong> songs;
+    private String type; // single, ep, lp
+    private List<AlbumSong> songs = new ArrayList<>();
 
-    // Nested class to hold songIDs and ordering within an album
-    public static class AlbumSong {
-        private String songId;
-        private int order;
-
-        public AlbumSong() {}
-        
-        public AlbumSong(String songId, int order) {
-            this.songId = songId;
-            this.order = order;
-        }
-        // Constructors
-        public void setSongId(String songId) {
-            this.songId = songId;
-        }
-        public String getSongId() {
-            return songId;
-        }
-        public void setOrder(int order) {
-            this.order = order;
-        }
-        public int getOrder() {
-            return order;
-        }
+    public String getId() {
+        return id;
     }
 
-    public Albums() {}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-    public Albums(String title, String artist, List<AlbumSong> songs, String type) {
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
         this.title = title;
-        this.artist = artist;
-        this.songs = songs;
-        this.type = type;
+    }
+
+    public String getArtist() {
+        return artist;
     }
 
     public void setArtist(String artist) {
         this.artist = artist;
     }
-    public String getArtist() {
-        return artist;
-    }
-    public void setId(String id) {
-        this.id = id;
-    }
-    public String getId() {
-        return id;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    public String getTitle() {
-        return title;
-    }
-    public void setSongs(List<AlbumSong> songs) {
-        this.songs = songs;
-    }
-    public List<AlbumSong> getSongs() {
-        return songs;
-    }
-    public void setType(String type) {
-        this.type = type;
-    }
+
     public String getType() {
         return type;
     }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public List<AlbumSong> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(List<AlbumSong> songs) {
+        this.songs = songs;
+    }
+
+    public static class AlbumSong {
+        private String songId;   // references Songs.id
+        private String title;    // store title so album retrieval is fast
+        private int order;       // track number
+
+
+        public AlbumSong() {}
+
+        public AlbumSong(String songId, String title, int order) {
+            this.songId = songId;
+            this.title = title;
+            this.order = order;
+        }
+
+         public String getSongId() {
+            return songId;
+        }
+
+        public void setSongId(String songId) {
+            this.songId = songId;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public int getOrder() {
+            return order;
+        }
+
+        public void setOrder(int order) {
+            this.order = order;
+        }
+    }
 }
+
 
 
 
