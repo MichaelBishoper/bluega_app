8a12194bca71c89a6bc7fe4bde34dd1dbe5f5b91
MichaelBishoper <xaverio.chung@student.sgu.ac.id>
2025-12-17 21:29:26 +0700

    Added find album by userId
    ROUTE: api/albums/user/{userId}

--- backend/musicplayer/src/main/java/com/musicplayer/musicplayer/controller/AlbumsController.java
+++ backend/musicplayer/src/main/java/com/musicplayer/musicplayer/controller/AlbumsController.java
@@ -1,19 +1,28 @@
 package com.musicplayer.musicplayer.controller;
 
+import java.io.IOException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.musicplayer.musicplayer.model.Songs; // Import Songs model
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.musicplayer.musicplayer.model.Albums;
-import com.musicplayer.musicplayer.service.AlbumsService;
-import com.musicplayer.musicplayer.service.SongsService;
-import com.musicplayer.musicplayer.service.S3Service;
+import com.musicplayer.musicplayer.model.Songs; // Import Songs model
 import com.musicplayer.musicplayer.repository.AlbumsRepository;
-
-import java.io.IOException;
-import java.util.List;     
+import com.musicplayer.musicplayer.service.AlbumsService;
+import com.musicplayer.musicplayer.service.S3Service;
+import com.musicplayer.musicplayer.service.SongsService;     
 
 @RestController
 @RequestMapping("api/albums")
@@ -45,6 +54,11 @@ public class AlbumsController {
     @GetMapping("/{id}/songs") // Album's ID NOT Song's
     public List<Songs> getSongsFromAlbum(@PathVariable String id) {
         return albumsService.getSongsFromAlbum(id);
+    }
+
+    @GetMapping("user/{userId}")
+    public List<Albums> getAlbumsByUser(@PathVariable String userId) {
+        return albumsService.getAlbumsByUserId(userId);
     }
 
     @PostMapping // Create Container Album
